9dc5628ffc6fe8ccfed8f927fa4a0dbf394d0902
Chenxi <chenxi.whitehouse@gmail.com>
2022-09-14 13:25:06 +0100

    Update download_weights.py

--- download_weights.py
+++ download_weights.py
@@ -1,25 +1,10 @@
 import os
-# import argparse
 import torch
 from diffusers import StableDiffusionPipeline, DDIMScheduler
 
 
-# if __name__ == "__main__":
-
-
-
 model_id = "hakurei/waifu-diffusion"
 cache_dir = "waifu-diffusion-cache"
-# os.makedirs(cache_dir, exist_ok=True)
-
-
-# pipe = StableDiffusionPipeline.from_pretrained(
-#     "CompVis/stable-diffusion-v1-4",
-#     cache_dir="waifu-diffusion-cache",
-#     revision="fp16",
-#     torch_dtype=torch.float16,
-#     use_auth_token=sys.argv[1],
-# )
 
 pipe = StableDiffusionPipeline.from_pretrained(
     model_id,
@@ -34,5 +19,3 @@ pipe = StableDiffusionPipeline.from_pretrained(
         set_alpha_to_one=False,
     ),
 )
-
-# print("All done!") # The permission of the cache-dir may need to change for the demo
